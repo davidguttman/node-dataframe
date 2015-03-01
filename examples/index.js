3043596638d8669dffbbcc30a751ac2dcef96627
David Guttman <david@davidguttman.com>
2015-03-01 13:54:47 -0800

    fiat lux (again) + getResults() working

--- examples/index.js
+++ examples/index.js
@@ -1,1 +1,59 @@
-var Dataframe = require('..')
+var DataFrame = require('..')
+
+var data = require('./data.json')
+
+var dimensions = [
+  {value: 'firstName', title: 'First Name'},
+  {value: 'lastName', title: 'Last Name'},
+  {value: 'state', title: 'State'},
+  {value: function(row) {
+    return row.transaction.business
+  }, title: 'Business'},
+  {value: function(row) {
+    return row.transaction.type
+  }, title: 'Transaction Type'}
+]
+
+var reduce = function(row, memo) {
+  memo.count = (memo.count || 0) + 1
+  memo.amountTotal = (memo.amountTotal || 0) + parseFloat(row.transaction.amount)
+  return memo
+}
+
+var calculations = [
+  {
+    title: 'Count',
+    value: 'count'
+  },
+  {
+    title: 'Amount',
+    value: 'amountTotal',
+    template: function(val, row) {
+      return '$' + val.toFixed(2)
+    }
+  },
+  {
+    title: 'Avg Amount',
+    value: function(row) {
+      return row.amountTotal / row.count
+    },
+    template: function(val, row) {
+      return '$' + val.toFixed(2)
+    }
+  }
+]
+
+var df = DataFrame({
+  rows: data,
+  dimensions: dimensions,
+  reduce: reduce,
+  calculations: calculations
+})
+
+var results = df.calculate({
+  dimensions: ['First Name', 'Last Name'],
+  sortBy: 'Amount',
+  sortDir: 'desc'
+})
+
+console.log('results', results)
---
+++ index.js
@@ -0,0 +1,136 @@
+var _ = require('lodash')
+
+module.exports = function(opts) {return new DataFrame(opts)}
+
+function DataFrame (opts) {
+  this.rows = opts.rows
+  this.dimensions = opts.dimensions
+  this.reduce = opts.reduce
+  this.calculations = opts.calculations
+  this.cache = {}
+
+  return this
+}
+
+DataFrame.prototype.calculate = function(opts) {
+  this.activeDimensions = opts.dimensions
+  this.sortBy = opts.sortBy
+  this.sortDir = opts.sortDir
+
+  var results = this.getResults()
+  return results
+}
+
+DataFrame.prototype.getResults = function() {
+  var self = this
+
+  var columns = this.getColumns()
+  var activeDimensions = this.activeDimensions
+  var reduce = this.reduce
+
+  var results = {}
+  var setKeyCache = {}
+
+  this.rows.forEach(function(row) {
+    var setKeys = self.createSetKeys(activeDimensions, row)
+    var curLevel = results
+
+    setKeys.forEach(function(setKey, iLevel) {
+      if (!curLevel[setKey]) {
+        curLevel[setKey] = {value: {}, subDimensions: {}, key: setKey}
+
+        self.calculations.forEach(function(calc) {
+          curLevel[setKey].value[calc.title] = 0
+        })
+      }
+
+      var result = curLevel[setKey].value
+
+      if (!self.cache[setKey]) {
+        setKeyCache[setKey] = result
+
+
+        var dimensionVals = parseSetKey(setKey)
+        _.extend(result, dimensionVals)
+        _.extend(result, reduce(row, result))
+      } else {
+        curLevel = curLevel[setKey].subDimensions
+      }
+
+      curLevel = curLevel[setKey].subDimensions
+    })
+  })
+
+  _.each(setKeyCache, function(cache, key) {
+    self.cache[key] = cache
+  })
+
+  return results
+
+}
+
+DataFrame.prototype.getColumns = function() {
+  var columns = []
+
+  this.dimensions.forEach(function(d) {
+    columns.push({type: 'dimension', title: d, value: d})
+  })
+
+  this.calculations.forEach(function(c) {
+    columns.push({
+      type:'calculation', title: c.title, template: c.template, value: c.value
+    })
+  })
+
+  return columns
+}
+
+DataFrame.prototype.createSetKeys = function(dimensions, row) {
+  var keys = []
+
+  for (var i = 0; i < dimensions.length; i++) {
+    var sds = dimensions.slice(0, i+1)
+    keys.push(this.createSetKey(sds, row))
+  }
+
+  return keys
+}
+
+DataFrame.prototype.createSetKey = function (dimensions, row) {
+  var self = this
+
+  var key = ''
+  _.sortBy(dimensions).forEach(function(dTitle) {
+    var dimension = self.findDimension(dTitle)
+    key += [dTitle, getValue(dimension, row)].join('\xff') + '\xff'
+  })
+  return key
+}
+
+DataFrame.prototype.findDimension = function (title) {
+  return _.find(this.dimensions, function(d) {
+    return d.title === title
+  })
+}
+
+function parseSetKey (setKey) {
+  var parsed = {}
+  var kvPairs = setKey.split('\xff')
+  for (var i = 0; i < kvPairs.length; i += 2) {
+    var dTitle = kvPairs[i]
+    var dVal = kvPairs[i+1]
+    if (dTitle) parsed[dTitle] = dVal
+  }
+  return parsed
+}
+
+function getValue (col, row) {
+  if (col == null) return null
+  var val
+  if (typeof col.value === 'string') {
+    val = row[col.value]
+  } else {
+    val = col.value(row)
+  }
+  return val
+}
